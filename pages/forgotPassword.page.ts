8a2e50d725879b6c76f7d6552e32e8e559c483f0
nemowbray <nemowbray#gmail.com>
2023-05-23 14:32:37 -0600

    Updated tests for login

--- pages/forgotPassword.page.ts
+++ pages/forgotPassword.page.ts
@@ -1,13 +1,15 @@
 import { Locator, Page } from "@playwright/test";
 
-export default class landingPage {
+export default class resetPassword {
 readonly page: Page;
 readonly emailAddress: Locator;
 readonly confirmResetButton: Locator;
+readonly confirmEmailSent: Locator;
 
 constructor(page:Page) {
     this.page = page;
     this.emailAddress = page.getByLabel('Email address');
-    this.confirmResetButton = page.getByRole('button', { name: 'Retrieve Password' })   
+    this.confirmResetButton = page.getByRole('button', { name: 'Retrieve Password' });   
+    this.confirmEmailSent = page.locator('#center_column');// Fix locator when defect is fixed
 }
 }
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,31 +1,55 @@
-import { test, expect } from '@playwright/test';
+import resetPassword from '@pages/forgotPassword.page';
+import landingPage from '@pages/landingPage.page';
+import loginOrRegisterUser from '@pages/login.page';
+import userAccount from '@pages/myAccount.page';
+import { test, expect, Page } from '@playwright/test';
 
-test('User Can login', async ({ page }) => {
+let page: Page;
+let homePage: landingPage;
+let login: loginOrRegisterUser;
+let forgotPassword: resetPassword;
+let myAccount: userAccount;
+
+test('User Can login', async ({ browser }) => {
+  page = await browser.newPage();
   await page.goto('/');
-
-
+  await page.waitForLoadState();
+  homePage = new landingPage(page);
+  await homePage.signInLink.click();
+  await expect(page).toHaveURL(/.*my-account/);
+  login = new loginOrRegisterUser(page);
+  await login.registeredEmailAddress.fill('nemowbray+2@gmail.com');
+  await login.password.fill('mypassword');
+  await login.signInButton.click();
+  await expect(page).toHaveURL(/.*my-account/);
+  await expect(homePage.userAccountLink).toBeVisible();
 });
 
-test('User cannot login when using incorrect password', async ({ page }) => {
+test('User cannot login when using incorrect password', async ({ browser }) => {
+  page = await browser.newPage();
   await page.goto('/');
+  await page.waitForLoadState();
+  homePage = new landingPage(page);
+  await homePage.signInLink.click();
+  await expect(page).toHaveURL(/.*my-account/);
+  login = new loginOrRegisterUser(page);
+  await login.registeredEmailAddress.fill('nemowbray+2@gmail.com');
+  await login.password.fill('password');
+  await login.signInButton.click()
+  await expect(login.invalidPassword).toContainText('Authentication failed.');
+  });
 
-
-});
-
-test('User can update password if they forgot their password', async ({ page }) => {
+test.fixme('User can update password if they forgot their password', async ({ browser }) => {
+  page = await browser.newPage();
   await page.goto('/');
-
-
-});
-
-test('User can update email address', async ({ page }) => {
-  await page.goto('/');
-
-
-});
-
-test('Validate API', async ({ page }) => {
-  await page.goto('/');
-
-
+  await page.waitForLoadState();
+  homePage = new landingPage(page);
+  await homePage.signInLink.click();
+  await expect(page).toHaveURL(/.*my-account/);
+  login = new loginOrRegisterUser(page);
+  await login.forgotPasswordLink.click();
+  await forgotPassword.emailAddress.fill('nemowbray+3@gmail.com');
+  await forgotPassword.confirmResetButton.click();
+  //TODO after defect is fixed
+  //await expect(forgotPassword.confirmEmailSent).toHaveText('Email has been sent to the email address with instructions to reset your password');
 });
